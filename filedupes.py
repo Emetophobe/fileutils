d99770e2ef219f9e114fe3dcb5cf558b356ae09c
Mike Cunningham <Mike933@gmail.com>
2023-01-09 11:21:04 -0500

    Updated to use new filehasher functions.
    Also switched to findfiles.walk_tree instead of os.walk().
    
    Added support for filtering symlinks and dotfiles.

--- filedupes.py
+++ filedupes.py
@@ -4,101 +4,116 @@
 
 
 import os
+import sys
 import time
 import hashlib
 import argparse
-import textwrap
 
 from collections import defaultdict
-from filehasher import create_hash
+
+from findfiles import walk_tree
+from filehasher import hash_file, format_algorithms
 
 
 # Default hashlib algorithm
-# See hashlib.algorithms_guaranteed and hashlib.algorithms_available for a complete list
 DEFAULT_ALGORITHM = 'sha3_256'
 
 
-def find_dupes(directory, algorithm, excludes=None):
-    """Create a dictionary of duplicate hashes (keys) and filenames (values). """
+def find_dupes(directory, algorithm=None, excludes=None, dotfiles=False, symlinks=False):
+    """ Find duplicate files using a hashlib algorithm. """
 
-    excludes = excludes or []
-    hashes = defaultdict(list)
+    if not algorithm:
+        algorithm = DEFAULT_ALGORITHM
 
-    # Walk the entire directory tree and hash every file
-    for root, dirs, files in os.walk(os.path.abspath(directory), topdown=True):
-        dirs[:] = [d for d in dirs if d not in excludes]
-        for filename in files:
-            filename = os.path.join(root, filename)
-            try:
-                # Add the filename and its hash to the dictionary
-                digest = create_hash(filename, algorithm)
-                hashes[digest].append(filename)
-            except OSError as e:
-                print(f'Error reading file: {e.filename} ({e.strerror})')
+    if not excludes:
+        excludes = []
 
-    # Return a new dict with only hashes that have multiple values (duplicates)
+    # Hash every file in the directory
+    hashes = defaultdict(list[str])
+    for entry in walk_tree(directory, excludes=excludes, symlinks=symlinks,
+                           dotfiles=dotfiles, recursive=True):
+        try:
+            digest = hash_file(entry.path, algorithm)
+            hashes[digest].append(entry.path)
+        except OSError as e:
+            print(f'Error reading {e.filename} ({e.strerror})', file=sys.stderr)
+
+    # Return a subset of the dictionary with duplicate hashes
     return {k: v for k, v in hashes.items() if len(v) > 1}
 
 
 def main():
-    supported_algorithms = textwrap.fill(', '.join(sorted(hashlib.algorithms_available)), 70)
-
     parser = argparse.ArgumentParser(
-        description='Find duplicate files by comparing checksums.',
-        epilog=f'List of supported algorithms:\n\n{supported_algorithms}',
+        description="Find duplicate files using one of Python's built-in hashing algorithm.",
+        epilog=f'List of supported algorithms:\n\n{format_algorithms()}',
         formatter_class=argparse.RawTextHelpFormatter
     )
 
     parser.add_argument(
         'directory',
-        help='top level directory to search'
+        help='top level search directory',
+        type=os.path.abspath  # force absolute paths
     )
 
     parser.add_argument(
-        '-a',
-        '--algorithm',
+        '-a', '--algorithm',
+        help='specify a hashlib algorithm (default: %(default)s)',
         metavar='algorithm',
         type=str.lower,  # force lowercase
         default=DEFAULT_ALGORITHM,
-        help='specify a hashlib algorithm (default: %(default)s)',
     )
 
     parser.add_argument(
         '-e', '--excludes',
-        help='optional list of files or directories to exclude',
+        help='list of files and directories to exclude (default: None)',
         metavar='excludes',
         nargs='*',
+    )
+
+    parser.add_argument(
+        '-d', '--dotfiles',
+        help='include dotfiles (default: False)',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '-s', '--symlinks',
+        help='follow symlinks (default: False)',
+        action='store_true'
     )
 
     args = parser.parse_args()
 
     # Make sure the directory exists
     if not os.path.isdir(args.directory):
-        parser.error(f'Invalid directory: {args.directory}')
+        parser.error('Invalid directory.')
 
     # Make sure the algorithm is supported
     if args.algorithm not in hashlib.algorithms_available:
-        parser.error(f'Invalid algorithm: {args.algorithm}. Use --help for a list of supported algorithms.')
+        parser.error(f'Invalid algorithm: {args.algorithm}.')
 
-    # Get duplicate files
-    print('Searching for duplicates. This may take a while...', flush=True)
-    start_time = time.perf_counter()
-    dupes = find_dupes(args.directory, args.algorithm, args.excludes)
-    elapsed_time = time.perf_counter() - start_time
+    # Find duplicate files
+    try:
+        print('Searching for duplicates. This may take a while...', flush=True)
+        start_time = time.perf_counter()
+        dupes = find_dupes(**vars(args))
+        elapsed_time = time.perf_counter() - start_time
+    except OSError as e:
+        print(f'Error reading {e.filename} ({e.strerror})', file=sys.stderr)
+        return 1
+    except KeyboardInterrupt:
+        print('Aborted.')
+        return 1
 
     # Print results
-    print()
     for digest, files in dupes.items():
-        print(digest)
+        print(f'\n{args.algorithm}: {digest}\n')
         for filename in files:
-            print('    ', filename)
-        print()
+            print(f'  {filename}')
 
-    print(f'Found {len(dupes)} duplicate hashes in {elapsed_time:.2f} seconds.')
+    print(f'\nFound {len(dupes):,} duplicate hashes in {elapsed_time:.2f} seconds.')
+    return 0
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Aborted.')
+    sys.exit(main())
